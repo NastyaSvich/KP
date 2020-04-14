ccd89fa11234258895de33b0ee918d79207603b1
DaryaDan <56804467+DaryaDan@users.noreply.github.com>
2020-04-14 13:01:59 +0300

    Update actions.js

--- КП/frontend/src/constants/actions.js
+++ КП/frontend/src/constants/actions.js
@@ -1,3 +1,12 @@
 export const LOGIN = "LOGIN"
 export const LOGOUT = "LOGOUT"
 export const EDIT = "EDIT"
+
+export const CREATE_FILM = "CREATE_FILM"
+export const GET_FILMS = "GET_FILMS"
+export const GET_FILM = "GET_FILM"
+export const RATE_FILM = "RATE_FILM"
+export const EDIT_FILM = "EDIT_FILM"
+export const DELETE_FILM = "DELETE_FILM"
+
+export const ERROR_MESSAGE = "ERROR_MESSAGE"
