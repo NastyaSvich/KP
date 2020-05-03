fd45a95ad83db0495066e20dfc6a85915eaf8750
DaryaDan <56804467+DaryaDan@users.noreply.github.com>
2020-05-03 10:36:58 +0300

    Update index.js

--- КП/frontend/src/api/index.js
+++ КП/frontend/src/api/index.js
@@ -1,3 +1,9 @@
+import axios from "axios"
+
+const instance = axios.create({
+  baseURL: "http://localhost:8080/"
+})
+
 instance.interceptors.response.use(res => {
   if (res.data.errorMessage) {
     alert(res.data.errorMessage)
